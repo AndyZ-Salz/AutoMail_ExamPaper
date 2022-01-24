a591a63506ec3f71f9a2a420a49d4bb32e8e6ddf
AndyZang-Salz <kaiyuezang@gmail.com>
2022-01-24 23:48:11 +0800

    init

---
+++ Resouces/config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : config
+@Version : ??
+@Author  : Andy Zang
+@software: PyCharm
+@For     : Auto_Mail
+---------------------------------------
+"""
+
+# History:
+# 2022/1/24: Create
+
+project_name = "Test"
+exam_name = "AS GEO Paper1"
+score_file = r"Resources/AS_21W_Paper1.xlsx"
+mail_templet = r"Resources/mail_templet.txt"
