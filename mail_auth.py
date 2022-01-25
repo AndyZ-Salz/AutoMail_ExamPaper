668803ade340a93dbf436c7dc25195a490c94d00
AndyZang-Salz <kaiyuezang@gmail.com>
2022-01-25 14:32:48 +0800

    send Mail

---
+++ mail_auth.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : mail_auth
+@Version : ??
+@Author  : Andy Zang
+@software: PyCharm
+@For     : Auto_Mail
+---------------------------------------
+"""
+
+# History:
+# 2022/1/25: Create
+
+# 第三方 SMTP 服务
+mail_host="smtp.exmail.qq.com"  #设置服务器 server
+mail_port= 465 #端口 port
+mail_user="xxx"    #用户名 username
+mail_pass="xxx"   #口令 password
+
---
+++ mail_send_demo.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : mail_send
+@Version : ??
+@Author  : Andy Zang
+@software: PyCharm
+@For     : Auto_Mail
+---------------------------------------
+"""
+
+# History:
+# 2022/1/25: Create
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+from email.utils import formataddr
+import mail_auth
+
+if __name__ == '__main__':
+    sender = 'aa@bb.com'
+    receivers = ['cc@dd.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+    # 创建一个带附件的实例
+    message = MIMEMultipart()
+    message['From'] = formataddr(["sender",sender])
+    message['To'] = formataddr(["receive","cc@dd.com"])
+
+    subject = 'Python SMTP 邮件测试'
+    message['Subject'] = Header(subject, 'utf-8')
+
+    # 邮件正文内容 plain
+    message.attach(MIMEText('这是菜鸟教程Python 邮件发送测试……', 'plain', 'utf-8'))
+
+    # 构造附件1，传送当前目录下的 test.txt 文件
+    att1 = MIMEText(open('test.txt', 'rb').read(), 'base64', 'utf-8')
+    att1["Content-Type"] = 'application/octet-stream'
+    # 这里的filename可以任意写，写什么名字，邮件中显示什么名字
+    att1["Content-Disposition"] = 'attachment; filename="test.txt"'
+    message.attach(att1)
+    #
+    # # 构造附件2，传送当前目录下的 runoob.txt 文件
+    # att2 = MIMEText(open('runoob.txt', 'rb').read(), 'base64', 'utf-8')
+    # att2["Content-Type"] = 'application/octet-stream'
+    # att2["Content-Disposition"] = 'attachment; filename="runoob.txt"'
+    # message.attach(att2)
+
+    try:
+        smtpObj = smtplib.SMTP_SSL(mail_auth.mail_host, mail_auth.mail_port)
+        smtpObj.login(mail_auth.mail_user, mail_auth.mail_pass)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print("邮件发送成功")
+        smtpObj.quit()
+
+    except smtplib.SMTPException:
+        print("Error: 无法发送邮件")
+
